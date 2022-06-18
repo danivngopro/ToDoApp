c8fd673202b22d098cd7137b5ee751915916bf7f
daninvngopro <danivngopro@gmail.com>
2022-06-18 16:30:58 +0300

    editing the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,17 @@
-import './About.css';
+import "./About.css";
 
 function About() {
-    return (
-        <h1>About</h1>
-    );
-  }
-  
-  export default About;
+  return (
+    <>
+      <h1>About this app:</h1>
+      <h2>This app was created as my first official project in react that i have built on my own.</h2>
+      <h2>It features a sign in, sign out, register, and password recovery services to the users.</h2>
+      <h2>This app is a basic TODO list manager.</h2>
+      <h2>You can add, edit, remove and check a task</h2>
+      <h2>Creator: Daniel Ventura</h2>
+      <h2>All rights reserved</h2>
+    </>
+  );
+}
+
+export default About;
