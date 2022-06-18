056e23323e3900ae8f892dab225a3d4cf63b652f
daninvngopro <danivngopro@gmail.com>
2022-06-18 09:26:11 +0300

    added loading spinner

--- src/pages/ToDo.js
+++ src/pages/ToDo.js
@@ -1,9 +1,9 @@
-import './ToDo.css';
-import { useEffect, useState } from 'react';
-import { auth } from '../firebase/base';
-import { onAuthStateChanged, signOut } from 'firebase/auth';
-import { useNavigate } from 'react-router';
-import { render } from 'react-dom';
+import "./ToDo.css";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase/base";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useNavigate } from "react-router";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 function ToDo() {
   console.log(auth.currentUser);
@@ -14,26 +14,34 @@ function ToDo() {
 
   useEffect(() => {
     const authentication = onAuthStateChanged(auth, (user) => {
-      if (!user) router('/login')
+      if (!user) router("/login");
       setIsLoading(false);
     });
 
-    return authentication
-  })
-  //maybe use effect on is loading to re render and maybe just ask if isloading in return
-
+    return authentication;
+  });
 
   const handleSignOut = () => {
-    signOut(auth).then(res => {
-      router('/login')
-    }).catch(err => console.log(err));
-  }
+    signOut(auth)
+      .then((res) => {
+        router("/login");
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
-    <>
-      {auth.currentUser ? <h3>Hello {auth.currentUser.displayName}</h3> : null}
-      <button onClick={handleSignOut}>Sign Out</button>
-    </>
+    <div className="container">
+      {!isLoading ? (
+        <>
+          <h3>Hello {auth.currentUser.displayName}</h3>
+          <button onClick={handleSignOut}>Sign Out</button>
+        </>
+      ) : (
+        <div className="loadingContainer">
+          <LoadingSpinner />
+        </div>
+      )}
+    </div>
   );
 }
 
